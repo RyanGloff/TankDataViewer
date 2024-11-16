4159a308d88e3d8b955ec93d5cc29d3a559dde5d
Ryan Gloff <ryan@ryangloff.com>
2024-11-16 17:20:31 -0500

    added paramter id to fetchParameterReadings

--- scripts/fetchParameterReadings.js
+++ scripts/fetchParameterReadings.js
@@ -1,19 +1,24 @@
 import usePgClient from './usePgClient.js';
 import { fileURLToPath } from 'url';
 
-export default function fetchParameterReadings(pgClient, start, end) {
+export default function fetchParameterReadings(pgClient, start, end, parameterId) {
   const sql = `
-    SELECT * FROM tank_data_schema.parameter_reading WHERE time > $1 AND time < $2;
+    SELECT * FROM tank_data_schema.parameter_reading WHERE time > $1 AND time < $2${parameterId ? ` AND parameter_id = $3` : ''};
   `;
+  console.log(sql);
   const params = [ start.toISOString(), end.toISOString() ];
+  if (parameterId) {
+    params.push(parameterId);
+  }
   return pgClient.query(sql, params).then(res => res.rows);
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
   const defaultTime = 7 * 24 * 60 * 60 * 100;
-  const start = process.argv[2] || new Date(Date.now() - defaultTime);
-  const end = process.argv[3] || new Date(Date.now());
+  const parameterId = process.argv[2];
+  const start = process.argv[3] || new Date(Date.now() - defaultTime);
+  const end = process.argv[4] || new Date(Date.now());
   console.log(start);
   console.log(end);
-  usePgClient('tank_data_injector', pgClient => fetchParameterReadings(pgClient, start, end).then(readings => console.log(readings)));
+  usePgClient('tank_data_injector', pgClient => fetchParameterReadings(pgClient, start, end, parameterId).then(readings => console.log(readings)));
 }
