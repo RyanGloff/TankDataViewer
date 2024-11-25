230a0aa230bd746faaaaf72a2ac54abdf6e43192
Ryan Gloff <ryan@ryangloff.com>
2024-11-24 21:23:23 -0500

    Gave main.js a better name

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,6 +1,6 @@
 WD=/home/ryan/tank_data_poller
 ENV_FILE=${WD}/prod.env
-MAIN_JS_FILE=${WD}/scripts/main.js
+MAIN_JS_FILE=${WD}/scripts/fetchAndStoreApexData.js
 
 . ${ENV_FILE}
 node ${MAIN_JS_FILE}
