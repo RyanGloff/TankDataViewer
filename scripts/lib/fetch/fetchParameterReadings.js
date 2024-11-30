3adec42246ee4eb67d9be0eb9ca3b11e36e56c7a
Ryan Gloff <ryan@ryangloff.com>
2024-11-30 17:27:35 -0500

    Made filter options optional and changed to passing in an options parameter

--- scripts/lib/fetch/fetchParameterReadings.js
+++ scripts/lib/fetch/fetchParameterReadings.js
@@ -1,24 +1,42 @@
 import usePgClient from '../usePgClient.js';
 import { fileURLToPath } from 'url';
 
-export default function fetchParameterReadings(pgClient, start, end, parameterId) {
-  const sql = `
-    SELECT * FROM tank_data_schema.parameter_reading WHERE time > $1 AND time < $2${parameterId ? ` AND parameter_id = $3` : ''};
-  `;
-  console.log(sql);
-  const params = [ start.toISOString(), end.toISOString() ];
+export default function fetchParameterReadings(pgClient, options) {
+  const { start, end, parameterId, tankId } = options;
+  let sql = `SELECT * FROM tank_data_schema.parameter_reading WHERE 1=1`;
+
+  let paramId = 1;
+  const params = [];
+  if (start) {
+    sql += ` AND time > $${paramId++}`;
+    params.push(start.toISOString());
+  }
+  if (end) {
+    sql += ` AND time < $${paramId++}`;
+    params.push(end.toISOString());
+  }
   if (parameterId) {
+    sql += ` AND parameter_id = $${paramId++}`;
     params.push(parameterId);
   }
+  if (tankId) {
+    sql += ` AND tank_id = $${paramId++}`;
+    params.push(tankId);
+  }
+
+  console.log(`Generated SQL: ${sql}`);
+  console.log(`Params: ${JSON.stringify(params)}`);
   return pgClient.query(sql, params).then(res => res.rows);
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
   const defaultTime = 7 * 24 * 60 * 60 * 100;
-  const parameterId = process.argv[2];
-  const start = process.argv[3] || new Date(Date.now() - defaultTime);
-  const end = process.argv[4] || new Date(Date.now());
-  console.log(start);
-  console.log(end);
-  usePgClient('tank_data_injector', pgClient => fetchParameterReadings(pgClient, start, end, parameterId).then(readings => console.log(readings)));
+  const options = {
+    parameterId: process.argv[2],
+    tankId: process.argv[3],
+    start: new Date(process.argv[4] || Date.now() - defaultTime),
+    end: new Date(process.argv[5] || Date.now()),
+  };
+  console.log(`Options: ${JSON.stringify(options)}`);
+  usePgClient('tank_data_injector', pgClient => fetchParameterReadings(pgClient, options).then(readings => console.log(readings)));
 }
