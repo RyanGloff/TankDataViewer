a8e2c035d29881e7a18f3a1a63f073622837f62d
Ryan Gloff <ryan@ryangloff.com>
2024-11-30 21:02:13 -0500

    Added delete reading script

---
+++ scripts/lib/delete/deleteReading.js
@@ -0,0 +1,14 @@
+import { fileURLToPath } from 'url';
+import usePgClient from '../usePgClient.js';
+
+export default function deleteReadings(pgClient, ids) {
+	const sql = `DELETE FROM tank_data_schema.parameter_reading where id IN ( ${ids.join(', ')} );`;
+  console.log(sql);
+	return Promise.resolve();//pgClient.query(sql).then(res => res.rows);
+}
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  const ids = process.argv.slice(2, process.argv.length);
+  console.log(`Ids: ${ids}`);
+	usePgClient('postgres', pgClient => deleteReadings(pgClient, ids));
+}
