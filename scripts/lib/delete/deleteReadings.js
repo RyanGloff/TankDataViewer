543c35c5100e23e9fc6fdcbe9df23ce0944b4df0
Ryan Gloff <ryan@ryangloff.com>
2024-12-01 11:12:00 -0500

    Fixed name of deleteReadings

--- scripts/lib/delete/deleteReadings.js
+++ scripts/lib/delete/deleteReadings.js
@@ -4,7 +4,7 @@ import usePgClient from '../usePgClient.js';
 export default function deleteReadings(pgClient, ids) {
 	const sql = `DELETE FROM tank_data_schema.parameter_reading where id IN ( ${ids.join(', ')} );`;
   console.log(sql);
-	return Promise.resolve();//pgClient.query(sql).then(res => res.rows);
+	return Promise.resolve(ids);//pgClient.query(sql).then(res => res.rows);
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
