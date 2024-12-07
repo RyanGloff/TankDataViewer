c64b78c2c461bb4d6c60238ce078434dee816bd4
Ryan Gloff <ryan@ryangloff.com>
2024-12-07 10:42:01 -0500

    Relocated logs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ cp -r . /opt/tankDataViewer
 # Update crontab
 MAIN_SCRIPT_PATH=/etc/tankDataViewer/scripts/main.sh
 BACKUP_SCRIPT_PATH=/etc/tankDataViewer/scripts/backup.sh
+LOG_DIR=/var/log/tankDataViewer
 ## Remove existing to avoid duplication
 crontab -l | grep -v '${MAIN_SCRIPT_PATH}' | crontab -
 crontab -l | grep -v '${BACKUP_SCRIPT_PATH}' | crontab -
@@ -11,8 +12,8 @@ crontab -l | grep -v '${BACKUP_SCRIPT_PATH}' | crontab -
 ## Add
 crontab -l > tankDataViewerCron
 cat <<EOL >> tankDataViewerCron
-* * * * * ${MAIN_SCRIPT_PATH} >> /home/ryan/tank_data_poller/logs/poller.txt 2>&1
-0 0 * * * ${BACKUP_SCRIPT_PATH} >> /home/ryan/tank_data_poller/logs/backup-log.txt 2>&1
+* * * * * ${MAIN_SCRIPT_PATH} >> ${LOG_DIR}/poller.txt 2>&1
+0 0 * * * ${BACKUP_SCRIPT_PATH} >> ${LOG_DIR}/backup-log.txt 2>&1
 EOL
 crontab -u ryan tankDataViewerCron
 rm tankDataViewerCron
