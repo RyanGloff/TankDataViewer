66de4109b9e978d433d1ed022ed54438df440e87
Ryan Gloff <ryan@ryangloff.com>
2024-12-21 12:23:12 -0500

    Added JSDoc to PromptFromList

--- scripts/lib/prompt/promptFromList.js
+++ scripts/lib/prompt/promptFromList.js
@@ -1,5 +1,14 @@
 import performQuestion from './performQuestion.js';
 
+/**
+ Prompts user to pick from a list of data objects
+ @param {listName} the name of the list of objects
+ @param {list} the list of objects
+ @param {nameField} the field in the objects to list out as an identifying name
+ @param {dataName} the field name to store the result in the data object
+ @param {data} the object of data collected. Also the place where the result is stored
+ @returns a promise for the data object
+ */
 export default function promptFromList(listName, list, nameField, dataName, data) {
   const header = `----- ${listName} -----`;
   console.log(header);
