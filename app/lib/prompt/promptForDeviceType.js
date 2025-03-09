bd40cd1c52c4acbfe84ee78f4c139dc6117c6836
Ryan Gloff <ryan@ryangloff.com>
2025-03-09 11:20:40 -0400

    Removed left over device code from former feature

--- app/lib/prompt/promptForDeviceType.js
+++
@@ -1,7 +0,0 @@
-import fetchDeviceTypes from '../fetch/fetchDeviceTypes.js';
-import promptFromList from './promptFromList.js';
-
-export default function promptForParameter(pgClient, dataName, data) {
-  return fetchDeviceTypes(pgClient)
-  .then(deviceTypes => promptFromList('Device Types', deviceTypes, 'name', dataName, data));
-}
