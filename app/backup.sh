bae8c358bbb4204362c48496723ac331956b8802
Ryan Gloff <ryan@ryangloff.com>
2025-03-19 20:46:37 -0400

    Removed old files

--- app/backup.sh
+++
@@ -1,9 +0,0 @@
-`cat /opt/tankDataViewer/scripts/deployment.env`
-
-BACKUP_PATH=${BACKUP_DIR}/backup-${DATE}.sql
-DATE=`date '+%Y-%m-%d'`
-export PGPASSWORD=docker
-
-echo "Starting dump for ${DATE}"
-pg_dump -U postgres -h localhost -d tank_data > $BACKUP_PATH
-echo "Ended dump for ${DATE}"
--- app/backupDatabase.js
+++
@@ -1,5 +0,0 @@
-import { execSync } from "child_process";
-
-export default function backupDatabase() {
-  console.log(execSync("pwd"));
-}
